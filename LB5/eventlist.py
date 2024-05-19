5d1c7f61c9341600cf51740a0a37078b3d198efc
Timur <113762194+TimurBarateliia@users.noreply.github.com>
2024-05-19 21:36:33 +0300

    LB5

--- LB5/eventlist.py
+++ LB5/eventlist.py
@@ -6,6 +6,7 @@ from check_rights import CheckRights
 import math
 import csv
 from io import StringIO, BytesIO
+from auth import check_perm
 
 
 bp_eventlist = Blueprint('eventlist', __name__, url_prefix='/eventlist')
@@ -55,9 +56,14 @@ def show():
         cursor.execute(query, (current_user.id, per_page, offset))
 
     events = cursor.fetchall()
+
+    user_visit_count_query = "SELECT COUNT(*) as count FROM eventlist WHERE user_id = %s"
+    cursor.execute(user_visit_count_query, (current_user.id,))
+
+    user_visit_count = cursor.fetchone().count
     cursor.close()
 
-    return render_template('visits/event.html', events=events, count=total_pages, page=page, per_page=per_page, offset=offset)
+    return render_template('visits/event.html', events=events, count=total_pages, page=page, per_page=per_page, offset=offset, user_visit_count=user_visit_count)
 
 @bp_eventlist.route('/show-path')
 @login_required
@@ -70,27 +76,18 @@ def show_path():
     return render_template('visits/event_path.html', events = events)
 
 @bp_eventlist.route('/show-path-user')
+@check_perm('show_log')
 @login_required
 def show_path_user():
     cursor = db.connection().cursor(named_tuple=True)
-    
-    if current_user.role_id == CheckRights.ADMIN_ROLE_ID:
-        query = '''
-        SELECT COUNT(*) as count, el.user_id, u.first_name, u.last_name
-        FROM eventlist el
-        LEFT JOIN users3 u ON el.user_id = u.id
-        GROUP BY el.user_id, u.first_name, u.last_name
-        '''
-        cursor.execute(query)
-    else:
-        query = '''
-        SELECT COUNT(*) as count, el.user_id, u.first_name, u.last_name
-        FROM eventlist el
-        LEFT JOIN users3 u ON el.user_id = u.id
-        WHERE el.user_id = %s
-        GROUP BY el.user_id, u.first_name, u.last_name
-        '''
-        cursor.execute(query, (current_user.id,))
+
+    query = '''
+    SELECT COUNT(*) as count, el.user_id, u.first_name, u.last_name
+    FROM eventlist el
+    LEFT JOIN users3 u ON el.user_id = u.id
+    GROUP BY el.user_id, u.first_name, u.last_name
+    '''
+    cursor.execute(query)
     
     events = cursor.fetchall()
     cursor.close()
@@ -129,17 +126,17 @@ def save_to_csv():
         '''
         cursor.execute(query, (current_user.id,))
         path_events = cursor.fetchall()
-        writer.writerow(['Журнал по страницам для пользователя'])
-        writer.writerow(['Количество посещений', 'Путь'])
-        for event in path_events:
-            writer.writerow([event[0], event[1]])
+        writer.writerow(['Log by page for user'])
+        writer.writerow(['Record Number', 'Number of visits', 'Path'])
+        for idx, event in enumerate(path_events, start=1):
+            writer.writerow([idx, event[1], event[0]])
 
     elif template == 'all':
         # Данные о всех посещениях
         cursor.execute('SELECT id, user_id, path FROM eventlist')
         logs = cursor.fetchall()
-        writer.writerow(['Журнал посещений'])
-        writer.writerow(['ID', 'ID Пользователя', 'Путь'])
+        writer.writerow(['Visit log'])
+        writer.writerow(['ID', 'User ID', 'Path'])
         for log in logs:
             writer.writerow([log[0], log[1], log[2]])
 
@@ -148,8 +145,8 @@ def save_to_csv():
         # Данные по пользователям
         cursor.execute('SELECT COUNT(*) as count, user_id FROM eventlist GROUP BY user_id')
         user_events = cursor.fetchall()
-        writer.writerow(['Журнал по пользователям'])
-        writer.writerow(['Количество посещений', 'ID Пользователя'])
+        writer.writerow(['User log'])
+        writer.writerow(['Number of visits', 'User ID'])
         for event in user_events:
             writer.writerow([event[0], event[1]])
 
@@ -158,10 +155,10 @@ def save_to_csv():
         # Данные по страницам
         cursor.execute('SELECT COUNT(*) as count, path FROM eventlist GROUP BY path')
         path_events = cursor.fetchall()
-        writer.writerow(['Журнал по страницам'])
-        writer.writerow(['Количество посещений', 'Путь'])
-        for event in path_events:
-            writer.writerow([event[0], event[1]])
+        writer.writerow(['Log by page'])
+        writer.writerow(['Number of visits', 'Path'])
+        for idx, event in enumerate(path_events, start=1):
+            writer.writerow([idx, event[1], event[0]])
 
     cursor.close()
 
